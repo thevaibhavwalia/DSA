2dd20640e02971f8b79953300387e8a4e3786095
thevaibhavwalia <129891354+thevaibhavwalia@users.noreply.github.com>
2024-02-08 14:29:55 +0530

    Update check-if-all-leaves-are-at-same-level.cpp

--- Easy/Check if all leaves are at same level/check-if-all-leaves-are-at-same-level.cpp
+++ Easy/Check if all leaves are at same level/check-if-all-leaves-are-at-same-level.cpp
@@ -1,90 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// Tree Node
-struct Node
-{
-    int data;
-    Node* left;
-    Node* right;
-};
-
-// Utility function to create a new Tree Node
-Node* newNode(int val)
-{
-    Node* temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
-    
-    return temp;
-}
-
-// Function to Build Tree
-Node* buildTree(string str)
-{   
-    // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
-    // string after spliting by space
-    vector<string> ip;
-    
-    istringstream iss(str);
-    for(string str; iss >> str; )
-        ip.push_back(str);
-        
-    // Create the root of the tree
-    Node* root = newNode(stoi(ip[0]));
-        
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-        
-    // Starting from the second element
-    int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-            
-        // Get the current node's value from the string
-        string currVal = ip[i];
-            
-        // If the left child is not null
-        if(currVal != "N") {
-                
-            // Create the left child for the current node
-            currNode->left = newNode(stoi(currVal));
-                
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-            
-        // For the right child
-        i++;
-        if(i >= ip.size())
-            break;
-        currVal = ip[i];
-            
-        // If the right child is not null
-        if(currVal != "N") {
-                
-            // Create the right child for the current node
-            currNode->right = newNode(stoi(currVal));
-                
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-    
-    return root;
-}
-
 
 // } Driver Code Ends
 /* The structure of the binary tree is as follows
@@ -95,6 +8,28 @@ struct Node
     Node* right;
 };
 */
+//Better 
+class Solution{
+  public:
+    bool help(Node* root, int &level,int temp){
+        if(!root)return 1;
+        if(!root->left && !root->right){
+            if(level!=-1){
+                return level==temp;
+            }else level=temp;
+        }
+        bool lh=help(root->left,level,temp+1);
+        bool rh=help(root->right,level,temp+1);
+        return rh&&lh;
+    }
+    
+    bool check(Node *root)
+    {
+        int level=-1,temp=0;
+        return help(root,level,temp);
+    }
+};
+//Brute
 
 class Solution{
   public:
