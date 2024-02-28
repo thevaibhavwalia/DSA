f88b1e137104aa8cc5f12da7debb1405f5c7d150
thevaibhavwalia <129891354+thevaibhavwalia@users.noreply.github.com>
2024-02-28 11:37:02 +0530

    Time: 7 ms (79.35%), Space: 20.1 MB (88.43%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -9,40 +9,28 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    int findBottomLeftValue(TreeNode* root) {
-        queue<TreeNode*> q;
-        q.push(root);
-
-int ans=-1;
-        while(!q.empty()){
-            int size=q.size();
-             ans=q.front()->val;
-            for(int i=0;i<size;i++){
-                auto front=q.front();
-                q.pop();
-                if(front->left){
-                    q.push(front->left);
-
-                }
-                if(front->right){
-                    q.push(front->right);
-                }
-
-            }
+    int ans=0;
+    int maxLevel;
+    void solve(TreeNode* root,int level){
+        if(!root){
+            return ;
         }
+        
+
+        if(level > maxLevel){
+            maxLevel=level;
+            ans=root->val;
+        }
+    solve(root->left,level+1);
+        
+        solve(root->right,level+1);
+        
+    }
+    int findBottomLeftValue(TreeNode* root) {
+        maxLevel=-1;
+        solve(root,0);
         return ans;
         
     }
