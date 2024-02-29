1f1dd9b5be0e33989df40a01b8d01cbc74c793ed
thevaibhavwalia <129891354+thevaibhavwalia@users.noreply.github.com>
2024-02-29 15:47:03 +0530

    Time: 4 ms (83.02%), Space: 9.4 MB (23.00%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -6,27 +6,20 @@ public:
         return (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u');
     }
     string reverseVowels(string s) {
-        int i=0;
-        int j=s.length()-1;
-        while(i < j){
-            while(i<j && !isVow(s[i])){
-                i++;
+        vector<char> v;
+        for(int i=0;i<s.length();i++){
+            if(isVow(s[i])){
+                v.push_back(s[i]);
             }
-             while(i<j && !isVow(s[j])){
-                j--;
+        } 
+        reverse(begin(v),end(v));
+    int j=0;
+        for(int i=0;i<s.length();i++){
+            if(isVow(s[i])){
+                s[i]=v[j];
+                j++;
             }
-            
-            if(i < j){
-                swap(s[i], s[j]);
-                i++;
-                j--;
-                
-            }
-        
-   
-          
         }
-        
         return s;
         
     }
