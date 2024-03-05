16c14fdf3dd26c341b4f7e6fa607c641926afe77
thevaibhavwalia <129891354+thevaibhavwalia@users.noreply.github.com>
2024-03-05 10:55:15 +0530

    Time: 11 ms (99.66%), Space: 14 MB (13.45%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -3,17 +3,22 @@ public:
     int minimumLength(string s) {
         int i = 0;
         int j = s.length() - 1;
-        
-        while (i < j && s[i] == s[j]) {
-            char ch = s[i];
-            while (i <= j && s[i] == ch) {
-                i++;
-            }
-            while (i <= j && s[j] == ch) {
-                j--;
+
+        while (i < j) {
+            if (s[i] == s[j]) {
+                char ch = s[i];
+                while (i <= j && s[i] == ch) {
+                    i++;
+                }
+                while (i <= j && s[j] == ch) {
+                    j--;
+                }
+
+            } else {
+                break;
             }
         }
-        
+
         return (j >= i) ? (j - i + 1) : 0;
     }
 };
