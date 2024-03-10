8cb6a2f152f575cd5d961e892fa55d46ff014b7c
thevaibhavwalia <129891354+thevaibhavwalia@users.noreply.github.com>
2024-03-10 13:13:14 +0530

    Time: 15 ms (93.65%), Space: 28.1 MB (14.00%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -1,37 +1,31 @@
-
 class BSTIterator {
 public:
-stack<int> inorder;
-int i=0;
+    vector<int> inorder;
+int i;
 void ino(TreeNode* root){
     if(!root){
         return;
     }
-    ino(root->right);
-    inorder.push(root->val);
     ino(root->left);
+    inorder.push_back(root->val);
+    ino(root->right);
 }
     BSTIterator(TreeNode* root) {
+        i=0;
         ino(root);
         
     }
     
     int next() {
-        int top=inorder.top();
-        inorder.pop();
-        return top;
+    int top= inorder[i];
+    i++;
+    return top;
        
     }
     
     bool hasNext() {
-        return !inorder.empty();
+        return i<inorder.size();
         
     }
 };
 
-/**
- * Your BSTIterator object will be instantiated and called as such:
- * BSTIterator* obj = new BSTIterator(root);
- * int param_1 = obj->next();
- * bool param_2 = obj->hasNext();
- */
