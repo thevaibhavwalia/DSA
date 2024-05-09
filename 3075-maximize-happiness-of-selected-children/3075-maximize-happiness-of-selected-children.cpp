61d973b8721a6b4a29e7eadef5a0d8c8b71f861d
thevaibhavwalia <129891354+thevaibhavwalia@users.noreply.github.com>
2024-05-09 16:42:59 +0530

    Time: 158 ms (67.75%), Space: 107.2 MB (32.91%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,32 +1,47 @@
+// class Solution {
+// public:
+//     long long maximumHappinessSum(vector<int>& happiness, int k) {
+//     priority_queue<long long > pq;
+//     for(auto it:happiness){
+//         pq.push(it);
+//     }
+//     long long ans=0;
+//     int add=0;
+//     while(!pq.empty() && k){
+//         auto top=pq.top();
+//         pq.pop();
+//         if(top-add < 0){
+//             continue;
+            
+//         }
+//         else{
+//             ans=ans+top-add;
+            
+//         }
+        
+//         add++;
+
+//         k--;
+//     }
+
+//     return ans;
+
+
+        
+//     }
+// };
+
 class Solution {
 public:
-    long long maximumHappinessSum(vector<int>& happiness, int k) {
-    priority_queue<long long > pq;
-    for(auto it:happiness){
-        pq.push(it);
-    }
-    long long ans=0;
-    int add=0;
-    while(!pq.empty() && k){
-        auto top=pq.top();
-        pq.pop();
-        if(top-add < 0){
-            continue;
+    long long maximumHappinessSum(vector<int>& hapiness, int k) {
+        long long ans = 0;
+        sort(hapiness.begin(),hapiness.end(),greater<int>());
+        int val=0;
+        for(int i=0; i<k; i++){
             
+            ans+=max(0,hapiness[i]-val);
+            val++;
         }
-        else{
-            ans=ans+top-add;
-            
-        }
-        
-        add++;
-
-        k--;
-    }
-
-    return ans;
-
-
-        
+        return ans;
     }
 };
