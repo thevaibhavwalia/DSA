0e24bd31fd7b1ae51a5803fc5f48f64f5515e132
thevaibhavwalia <129891354+thevaibhavwalia@users.noreply.github.com>
2024-06-27 10:46:00 +0530

    Time: 27 ms (72.93%), Space: 23.2 MB (74.86%) - LeetHub

--- 1757-minimum-jumps-to-reach-home/1757-minimum-jumps-to-reach-home.cpp
+++ 1757-minimum-jumps-to-reach-home/1757-minimum-jumps-to-reach-home.cpp
@@ -1,33 +1,45 @@
 class Solution {
 public:
-    int solve(unordered_set<int>& forbidden, unordered_map<int, unordered_map<int, int>>& memo, int pos, int a, int b, int x, bool back) {
- 
-        if (pos == x) return 0;
-        if (pos < 0 || pos > 6000 || forbidden.count(pos)) return INT_MAX;
-
- 
-        if (memo[pos][back] != 0) return memo[pos][back];
-
-
-        forbidden.insert(pos);
-
-
-        int forward = solve(forbidden, memo, pos + a, a, b, x, false);
-        int backward = back ? INT_MAX : solve(forbidden, memo, pos - b, a, b, x, true);
-
-        
-        forbidden.erase(pos);
-
-        
-        memo[pos][back] = min(forward, backward) == INT_MAX ? INT_MAX : 1 + min(forward, backward);
-
-        return memo[pos][back];
-    }
-
     int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
-        unordered_set<int> forbidden_set(forbidden.begin(), forbidden.end());
-        unordered_map<int, unordered_map<int, int>> memo;
-        int result = solve(forbidden_set, memo, 0, a, b, x, false);
-        return result == INT_MAX ? -1 : result;
+
+        unordered_set<int> visited(forbidden.begin(),forbidden.end());
+        
+        queue<pair<int,int>> q;  // here second value is what last move was taken
+        q.push({0,1});          // 1 for to the right and -1 for left
+        visited.insert(0);
+        
+        int level=0;
+        while(!q.empty())
+        {
+            level++;
+            int size=q.size();
+            
+            while(size--)
+            {
+                int u,lastMove;
+                tie(u,lastMove)=q.front();
+                q.pop();
+                
+                if(u==x)
+                    return level-1;
+                
+                int forward=u+a;
+                int backward=u-b;
+                
+                if(forward<=6000 && visited.find(forward)==visited.end())
+                {
+                    q.push({forward,1});
+                    visited.insert(forward);  
+                }
+                
+                if(lastMove==1 && backward>=0 && visited.find(backward)==visited.end() )
+                {
+                     q.push({backward,-1});
+                }
+                
+            }
+        }
+        
+        return -1;
     }
 };
