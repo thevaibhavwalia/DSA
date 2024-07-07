efeae63df76af10a1ac85091d80e3a3fcf2e151c
thevaibhavwalia <129891354+thevaibhavwalia@users.noreply.github.com>
2024-07-07 12:16:46 +0530

    Time: 19 ms (40%), Space: 33.2 MB (20%) - LeetHub

---
+++ 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> validStrings(int n) {
+        N = n;
+        vector<string> ans;
+        solve(ans, "", 0);
+        return ans;
+    }
+
+private:
+    int N;
+
+    bool one(const string& temp) {
+        for (int i = 1; i < temp.length(); i++) {
+            if (temp[i - 1] == '0' && temp[i] == '0') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void solve(vector<string>& ans, string temp, int idx) {
+        if (idx == N) {
+     
+                ans.push_back(temp);
+       
+            return;
+        }
+
+        if (temp.empty() || temp.back() == '1') {
+            solve(ans, temp + '0', idx + 1);
+            solve(ans, temp + '1', idx + 1);
+        } else if (temp.back() == '0') {
+            solve(ans, temp + '1', idx + 1);
+        }
+    }
+};
